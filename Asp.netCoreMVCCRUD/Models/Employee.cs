1bd91230aeba3ef470fac261b6e0dfafebef2772
David Poarch <davidpoarch@outlook.com>
2019-06-17 22:16:35 +0800

    Initial Files Upload

---
+++ Asp.netCoreMVCCRUD/Models/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.netCoreMVCCRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(250")]
+        [Required(ErrorMessage ="This field is required")]
+
+        [DisplayName("FULL NAME")]
+        public string name { get; set; }
+        [Column(TypeName =  "varchar(10")]
+
+        [DisplayName("EMP. CODE")]
+        public string code { get; set; }
+        [Column(TypeName =  "varchar(100")]
+
+        [DisplayName("POSITION")]
+        public string position { get; set; }
+        [Column(TypeName = "varchar(100")]
+
+        [DisplayName("OFFICE LOCATION")]
+        public string location { get; set; }
+
+    }
+}
